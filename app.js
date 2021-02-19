a5abc8338eb270eaafef01e406c1a3730d196a0a
Karel Haapasalo <karel.haapasalo@hotmail.com>
2021-02-19 15:56:19 +0200

    validation and schema added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -7,7 +8,8 @@ const pets = [
     { id: 1, name: 'Rover'},
     { id: 2, name: 'Whiskers'},
     { id: 3, name: 'Tipi'}
-]
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
@@ -23,10 +25,47 @@ app.get('/api/pets/:id', (req, res) => {
 });
 
 app.post('/api/pets', (req, res) => {
+
+    let owner_id = req.body.owner_id;
+    let animal = req.body.animal;
+    let petname = req.body.petname;
+    let color = req.body.color;
+    let avatar = req.body.avatar;
+    let food_storage = req.body.food_storage;
+
+    const schema = Joi.object({
+        owner_id: Joi.number().required(),
+        animal: Joi.string().required(),
+        petname: Joi.string().min(3).max(20).required(),
+        color: Joi.string().max(7),
+        avatar: Joi.string().max(10),
+        food_storage: Joi.number()
+    });
+
+    const result = schema.validate({ 
+        owner_id: owner_id,
+        animal: animal,
+        petname: petname,
+        color: color,
+        avatar: avatar,
+        food_storage: food_storage
+     });
+
+    if (result.error) {
+        res.status(400).send(result.error.message);
+        return;
+    }
+
     const pet = {
         id: pets.length + 1,
-        name: req.body.name
+        owner_id: owner_id,
+        animal: animal,
+        petname: petname,
+        color: color,
+        avatar: avatar,
+        food_storage: food_storage
     };
+    
     pets.push(pet);
     res.send(pet);
 });
